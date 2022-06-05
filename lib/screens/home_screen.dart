7e615eb752181e4f9c08b14076c5ce11196686f9
LeoGarroni <71300827+LeoGarroni@users.noreply.github.com>
2022-06-05 16:52:28 -0300

    descricao e icones

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,17 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:wood_analyzer/routes/routes.dart';
 import 'package:wood_analyzer/utils/app_colors.dart';
 import 'package:wood_analyzer/utils/dimensions.dart';
 import 'package:wood_analyzer/widgets/button_widget.dart';
 import 'package:wood_analyzer/widgets/text_container_widget.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -75,31 +80,66 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
           ),
-          SizedBox(
-            height: 30,
-          ),
-          Center(
+          SingleChildScrollView(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                SizedBox(
-                  height: Dimensions.height40,
-                ),
-                const Text(
-                  'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w400,
+                Container(
+                  padding: EdgeInsets.all(20),
+                  child: const Text(
+                    'O Wood Analyzer é um aplicativo com o objetivo de documentar diagnósticos para doenças na pele. Para utilizar, cadastre-se e faça o login com a sua conta. Após isso preencha os campos com as informações do paciente. Agora você terá que selecionar a imagem ou tirar uma nova para que seja analisada. Preencha os campos com as informações da pele do paciente. Agora podemos fazer a análise, selecione as opções do diagnóstico.',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w400,
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: Dimensions.height40,
                 ),
               ],
             ),
           ),
         ],
+      ),
+      bottomSheet: Container(
+        height: 50,
+        color: Colors.white,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                InkWell(
+                  child: Icon(
+                    FontAwesomeIcons.linkedin,
+                    color: AppColors.mainColor,
+                    size: 35,
+                  ),
+                  onTap: () =>
+                      launch('https://www.linkedin.com/in/lucas-cruzz/'),
+                ),
+              ],
+            ),
+            InkWell(
+              child: Icon(
+                FontAwesomeIcons.linkedin,
+                color: AppColors.mainColor,
+                size: 35,
+              ),
+              onTap: () =>
+                  launch('https://www.linkedin.com/in/matheus-fidelis/'),
+            ),
+            InkWell(
+              child: Icon(
+                FontAwesomeIcons.linkedin,
+                color: AppColors.mainColor,
+                size: 35,
+              ),
+              onTap: () =>
+                  launch('https://www.linkedin.com/in/leonardo-garroni'),
+            ),
+          ],
+        ),
       ),
     );
   }
